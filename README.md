ea5f4485021ca6a55d6817c9b1286bbe63f80f8a
Rich Hildred <rhildred@gmail.com>
2021-11-09 16:47:46 -0500

    Remove unneeded code from the contact mailer.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# contact page mailer
+# contact page Server Side
 
-I followed [this article](https://medium.com/@RistaSB/use-expressjs-to-send-mails-with-gmail-oauth-2-0-and-nodemailer-d585bba71343). That is up until the [oauth playground](https://developers.google.com/oauthplayground/) step. When you make your request for a refresh token you need to set the oauth2 configuration like so:
+This is a server page that responds to a post message and displays 
 
 ![set client id and secret](README_images/ScreenShotOfOauth2Config.png "set client id and secret")
 
--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-let auth = {
-    type: "oauth2",
-    user: process.env.USER,
-    clientId: process.env.CLIENTID,
-    clientSecret: process.env.CLIENTSECRET,
-    refreshToken: process.env.REFRESHTOKEN
-}
-try{
-    auth = require('./creds.json');
-    console.log("getting credentials from ./creds.json");
-}catch(e){
-    console.log(e.toString());
-    console.log("getting creds from environment variables");
-}
-
-const nodemailer = require('nodemailer');
 const express = require('express');
-const multer = require('multer');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -24,7 +7,6 @@ app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse multipart/form-data
-app.use(multer({ dest: __dirname + '/file/uploads/' }).any());
 
 // set up cors
 app.use(function (req, res, next) {
@@ -37,32 +19,15 @@ const port = process.env.PORT || 3002;
 
 app.post('/send', function (req, res) {
     const response = {
-        name: req.body.name,
-        email: req.body.email,
-        message: req.body.message
+        name: decodeURI(req.body.name),
+        email: decodeURI(req.body.email),
+        message: decodeURI(req.body.message)
     }
+    res.end(`
+Thank-you for your message ${req.body.name}
+    `);
 
-    const mailOptions = {
-        from: response.name,
-        "reply-to": response.email,
-        to: auth.user,
-        subject: 'My site contact from: ' + response.name,
-        text: req.body.message,
-        html: 'Message from: ' + response.name + '<br></br> Email: ' + response.email + '<br></br> Message: ' + response.message,
-    }; 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: auth,
-    }); 
-    transporter.sendMail(mailOptions, (err, response) => {
-        if (err) {
-            return console.log(err);
-        } else {
-            console.log(JSON.stringify(response));
-            res.end("thank-you for your message");
-        }
-    });
-})
+});
 
 // start the server
 app.listen(port, () => {
